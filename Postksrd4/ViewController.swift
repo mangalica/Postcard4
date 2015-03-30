a9b0e227547480a32011d5d31832f3fd893de632
Magdalena Kozomara <magdalenakozomara@Magdalenas-iMac.local>
2015-03-31 01:13:50 +0200

    Initial Commit

---
+++ Postksrd4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postksrd4
+//
+//  Created by Magdalena Kozomara on 31.03.15.
+//  Copyright (c) 2015 Vladimir Kozomara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
